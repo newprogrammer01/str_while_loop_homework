385ba46c4b5fe0b3a246d099747003ab4d9f604b
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-30 15:48:41 +0500

    08

--- While08.py
+++ While08.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    b=0
+    while a<len(s):
+        if s[a].isdigit():
+            b+=1
+        a+=1
+    return b-(s.count("0")+s.count('2')+s.count('4')+s.count('6')+s.count('8'))
+print(main("1234567890"))
