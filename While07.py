81ee36e2a7610c7a9f33dc05247dd1a6e5c194a2
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-30 15:43:52 +0500

    07

--- While07.py
+++ While07.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=0
+    b=0
+    while a<len(s):
+        if s[a].isdigit():
+            b+=1
+        a+=1
+    return b -(s.count('1')+s.count('3')+s.count('5')+s.count('7')+s.count('9'))
+print(main("1515"))
